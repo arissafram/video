65904c8ea31fa34db13090e66d2ca4ffa95d4392
Arissa Fram <arissafram@gmail.com>
2019-02-16 11:08:58 -0500

    initial control bar restructuring

--- src/HBO.js
+++ src/HBO.js
@@ -5,7 +5,25 @@ import 'video.js/dist/video-js.css';
 export default class HBOPlayer extends Component {
   
   componentDidMount() {
+
+    videojs.hook('beforesetup', () => {
+      videojs.getComponent('ControlBar').prototype.options_ = {
+        loadEvent: 'ready',
+        children: [
+          'progressControl',
+          'playToggle',
+          'currentTimeDisplay',
+          'remainingTimeDisplay',
+          'muteToggle',
+          'volumeControl',
+          'subsCapsButton',
+          'fullscreenToggle'
+        ]
+      }
+    })
+
     this.player = videojs(this.videoNode, this.props, () => console.log('player ready:', this))
+
     window.player = this.player
 
     this.player.on('ready', () => {
@@ -13,9 +31,16 @@ export default class HBOPlayer extends Component {
       this.player.volume(0)
     })
 
-    this.player.on('ended', () => {
-      this.player.posterImage.el_.style.display = "block"
-    })
+    // NOTE: re-ordering btns - https://github.com/videojs/video.js/issues/2673
+  //   videojs.getComponent('ControlBar').prototype.options_ = {
+  //     loadEvent: 'ready',
+  //     children: [
+  //       'muteToggle',
+  //       'playToggle',
+  //       'volumeControl',
+  //       'fullscreenToggle'
+  //     ]
+  //   }
   }
 
   comoponentWillUnmount() {
@@ -46,6 +71,15 @@ export default class HBOPlayer extends Component {
           className="videoNode video-js vjs-16-9 vjs-hbo">
             <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
         </video>
+        <ul>
+          <li>progress bar - position, color, hover</li>
+          <li>rewind 10 seconds btn</li>
+          <li>time remaining/elapsed</li>
+          <li>cc button</li>
+          <li>play button in bottom left corner</li>
+          <li>volume - right side, panel is vertical, colors</li>
+          <li>animated play/pause on video click</li>
+        </ul>
       </div>
     )
   }
